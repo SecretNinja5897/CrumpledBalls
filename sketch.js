a0d56ec83370c01d3aa3550287446d51d0cdba0d
SecretNinja5897 <73794490+SecretNinja5897@users.noreply.github.com>
2020-11-19 21:03:59 -0800

    Update sketch.js

--- sketch.js
+++ sketch.js
@@ -5,15 +5,26 @@ const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 
 var paper,ground,ceiling,wall1,wall2,trashBottom,trashSide1,trashSide2;
+var bin,binImage;
+
+
+function preload()
+{
+	binImage = loadImage("sprites/dustBin.png");
+}
 
 function setup() {
 	createCanvas(1920,700);
 
 
+	bin=createSprite(1345,310,200,22);
+	bin.addImage(binImage);
+	bin.scale=0.9;
+
 	engine = Engine.create();
 	world = engine.world;
 
-	paper=new Paper(25);
+	paper=new Paper(50);
 	ground=new Ground(965,height+23,width,20);
 	ceiling=new Ground(965,-10,width,20);
 	wall1=new Ground(-20,350,20,height);
@@ -31,19 +42,19 @@ function setup() {
 
 function draw() {
   rectMode(CENTER);
-  background(0);
+  background(255);
   
 	paper.display();
 	ground.display();
-	trashBottom.display();
-	trashBottom.velocityY=3;
 
+	trashBottom.display();
 	trashSide1.display();
 	trashSide2.display();
 
   keyPressed();
   drawSprites();
  
+
 }
 
 function keyPressed()
